fe284bafef3da4f05a4d66fc1d16dfb5e775bf79
IT24103669 <IT24103669@my.sliit.lk>
2024-11-18 12:28:57 +0530

    Add files via upload

---
+++ Q1A/IT24103669Lab3Q1A.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class RicePriceCalculator {
+    public static void main(String[] args) {
+       
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        System.out.print("Enter the price of 1kg of rice: ");
+        double pricePerKg = scanner.nextDouble();
+        
+        
+        System.out.print("Enter the number of kilograms of rice you want to buy: ");
+        double kilograms = scanner.nextDouble();
+        
+        
+        double totalAmount = pricePerKg * kilograms;
+        
+        
+        System.out.printf("The total amount to pay for %.2f kilograms of rice is: Rs %.2f\n", kilograms, totalAmount);
+        
+        
+        scanner.close();
+    }
+}
